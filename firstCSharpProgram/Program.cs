4907ef1ea7ef028874d6a2c7671c54d476e17cc9
Hitvardhan-Singh-Solanki <hitvardhan19@gmail.com>
2016-08-05 23:41:42 +0530

    initial Commit

---
+++ firstCSharpProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstCSharpProgram
+{   
+    /// <summary>
+    /// A Class That prints a message
+    /// </summary>
+    class Program
+    {   
+        /// <summary>
+        /// Main method to print the message
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature (Fahrenheit): ");
+            float originalFahrenheit = float.Parse(Console.ReadLine());
+            float toCelcius = ((originalFahrenheit - 32)/9)*5;
+            Console.WriteLine("\n Temperature in celcius is: "+toCelcius+"\n");
+        }
+    }
+}
